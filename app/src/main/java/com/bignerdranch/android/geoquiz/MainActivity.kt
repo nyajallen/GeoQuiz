055e250eb6fa582a7ff5eb2965c9cf20843554a0
Mark Hills <mhills@acm.org>
2020-05-20 09:07:43 -0400

    Track cheating status per question

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -102,7 +102,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (requestCode == REQUEST_CODE_CHEAT) {
-            quizViewModel.isCheater =
+            quizViewModel.hasCheatedOn[quizViewModel.currentIndex] =
                 data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
         }
     }
@@ -115,7 +115,7 @@ class MainActivity : AppCompatActivity() {
         val correctAnswer = quizViewModel.currentQuestionAnswer
 
         val messageResId = when {
-            quizViewModel.isCheater -> R.string.judgment_toast
+            quizViewModel.hasCheatedOn[quizViewModel.currentIndex] -> R.string.judgment_toast
             userAnswer == correctAnswer -> R.string.correct_toast
             else -> R.string.incorrect_toast
         }
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -14,8 +14,9 @@ class QuizViewModel : ViewModel() {
         Question(R.string.question_asia, true)
     )
 
+    val hasCheatedOn = MutableList(questionBank.size, { false })
+
     var currentIndex = 0
-    var isCheater = false
 
     val currentQuestionAnswer: Boolean
         get() = questionBank[currentIndex].answer
